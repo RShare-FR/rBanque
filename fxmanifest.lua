b43d5125407c22fc20e94705c41fbb45195f6acc
WirzRexTon <73710374+WirzRexTon@users.noreply.github.com>
2021-03-25 21:06:00 +0100

    bruh

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,33 +2,21 @@ fx_version 'adamant'
 game { 'gta5' }
 
 client_scripts {
-    "@RageUI/src/client/RMenu.lua",
-    "@RageUI/src/client/menu/RageUI.lua",
-    "@RageUI/src/client/menu/Menu.lua",
-    "@RageUI/src/client/menu/MenuController.lua",
-    "@RageUI/src/client/components/Audio.lua",
-    "@RageUI/src/client/components/Rectangle.lua",
-    "@RageUI/src/client/components/Screen.lua",
-    "@RageUI/src/client/components/Sprite.lua",
-    "@RageUI/src/client/components/Text.lua",
-    "@RageUI/src/client/components/Visual.lua",
-    "@RageUI/src/client/menu/elements/ItemsBadge.lua",
-    "@RageUI/src/client/menu/elements/ItemsColour.lua",
-    "@RageUI/src/client/menu/elements/PanelColour.lua",
-    "@RageUI/src/client/menu/items/UIButton.lua",
-    "@RageUI/src/client/menu/items/UICheckBox.lua",
-    "@RageUI/src/client/menu/items/UIList.lua",
-    "@RageUI/src/client/menu/items/UIProgress.lua",
-    "@RageUI/src/client/menu/items/UISlider.lua",
-    "@RageUI/src/client/menu/items/UISeparator.lua",
-    "@RageUI/src/client/menu/items/UISliderHeritage.lua",
-    "@RageUI/src/client/menu/items/UISliderProgress.lua",
-    "@RageUI/src/client/menu/panels/UIColourPanel.lua",
-    "@RageUI/src/client/menu/panels/UIGridPanel.lua",
-    "@RageUI/src/client/menu/panels/UIGridPanelHorizontal.lua",
-    "@RageUI/src/client/menu/panels/UIPercentagePanel.lua",
-    "@RageUI/src/client/menu/panels/UIStatisticsPanel.lua",
-    "@RageUI/src/client/menu/windows/UIHeritage.lua",
+    "src/client/RMenu.lua",
+    "src/client/menu/RageUI.lua",
+    "src/client/menu/Menu.lua",
+    "src/client/menu/MenuController.lua",
+
+    "src/client/components/*.lua",
+
+    "src/client/menu/elements/*.lua",
+
+    "src/client/menu/items/*.lua",
+
+    "src/client/menu/panels/*.lua",
+
+    "src/client/menu/windows/*.lua",
+
 }
 
 ------------------------TOUT action ce passe cotés client en gros juste pour le joueur
